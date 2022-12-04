1084e8da39a260c4e30bd69542c599e97b0d1631
Lourdes Dinesh <lourdes.dinesh@eka.care>
2022-12-04 16:05:17 +0530

    Rename landing activity layout

--- app/src/main/java/com/example/inztagram/controllers/LandingPage.java
+++ app/src/main/java/com/example/inztagram/controllers/LandingPage.java
@@ -22,7 +22,7 @@ public class LandingPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_landing_page);
 
         setElements();
         setOnClickListeners();
